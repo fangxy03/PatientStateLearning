60bbcfe2c86383fa6266f6f55a5b9a2fb0e2194d
fangxinyuan <1486762095@qq.com>
2026-07-30 10:20:54 +0800

    Update code

--- models/esi_classifier.py
+++ models/esi_classifier.py
@@ -6,25 +6,11 @@ class ESIClassifier(nn.Module):
 
     def __init__(
         self,
-        input_dim=512,
         state_dim=128,
         num_classes=5
     ):
 
         super().__init__()
-
-
-        self.state_projection = nn.Sequential(
-
-            nn.Linear(
-                input_dim,
-                state_dim
-            ),
-
-            nn.ReLU(),
-
-            nn.Dropout(0.3)
-        )
 
 
         self.classifier = nn.Sequential(
@@ -38,6 +24,7 @@ class ESIClassifier(nn.Module):
 
             nn.Dropout(0.3),
 
+
             nn.Linear(
                 64,
                 num_classes
@@ -47,10 +34,6 @@ class ESIClassifier(nn.Module):
 
     def forward(self,x):
 
-        clinical_state = self.state_projection(x)
+        logits = self.classifier(x)
 
-        logits = self.classifier(
-            clinical_state
-        )
-
-        return logits, clinical_state
+        return logits
